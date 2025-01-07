fe9cd5b81b237f51d5d1744202d313384379e99d
Eliecer Arteaga <earteaga.externo@surainvestments.com>
2025-01-07 19:08:29 -0300

    crud scaffolfing and setting up mongo db

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,20 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
+
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot(),
+    MongooseModule.forRoot(process.env.MONGO_URI),
+    // MongooseModule.forRoot('mongodb://localhost:27017'),
+    AuthModule],
+  controllers: [],
+  providers: [],
 })
-export class AppModule {}
+export class AppModule {
+  // constructor() {
+  //   console.log(process.env.MONGO_URI);
+  // }
+}
